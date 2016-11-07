05770947db8e9c1d4dc8897580eb1251aa154672
Brandon Espinoza <espinozab100@gmail.com>
2016-11-07 13:23:58 -0600

    Created new model with the pages attributes

--- db/migrate/20161103141340_create_logos.rb
+++
@@ -1,9 +0,0 @@
-class CreateLogos < ActiveRecord::Migration
-  def change
-    create_table :logos do |t|
-      t.string :name
-
-      t.timestamps null: false
-    end
-  end
-end
---
+++ db/migrate/20161107191354_create_homes.rb
@@ -0,0 +1,22 @@
+class CreateHomes < ActiveRecord::Migration
+  def change
+    create_table :homes do |t|
+      t.string :name
+      t.string :header_title
+      t.string :services_title
+      t.string :slogan_title
+      t.string :about_title
+      t.text :about_description
+      t.string :contact_title
+      t.string :contact_subtitle
+      t.string :fb_username
+      t.string :ig_username
+      t.string :twitter_username
+      t.string :in_username
+      t.string :pin_username
+
+
+      t.timestamps null: false
+    end
+  end
+end
