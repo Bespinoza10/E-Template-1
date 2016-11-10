48dc9d899aa8f6f8bf7512eb9d0ac82f252f0ff4
Brandon Espinoza <espinozab100@gmail.com>
2016-11-10 10:10:41 -0600

    Created Service Controller and Model. Functionality for the controller WIP

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161107191354) do
+ActiveRecord::Schema.define(version: 20161110160607) do
 
   create_table "contents", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20161107191354) do
     t.string   "pin_username"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
