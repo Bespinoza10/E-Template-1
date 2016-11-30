d83b6072b16b7310656c676ea4c24799e320a433
Brandon Espinoza <espinozab100@gmail.com>
2016-11-30 11:21:08 -0600

    Edit the header works with modal.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161128192430) do
+ActiveRecord::Schema.define(version: 20161130162243) do
 
   create_table "contents", force: :cascade do |t|
     t.string   "name"
