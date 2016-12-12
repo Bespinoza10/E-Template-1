5df42f3e964fcb649fb0f6173d667a4c7c17c8d9
Brandon Espinoza <espinozab100@gmail.com>
2016-12-12 09:31:47 -0600

    Removed default_url from header model

--- app/models/header.rb
+++ app/models/header.rb
@@ -1,4 +1,4 @@
 class Header < ActiveRecord::Base
-  has_attached_file :header_bg, styles: { large: "1920x1080>", medium: "300x300>", thumb: "100x100>" }, :default_url => '/images/default-header.jpg'
+  has_attached_file :header_bg, styles: { large: "1920x1080>", medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :header_bg, content_type: /\Aimage\/.*\z/
 end
