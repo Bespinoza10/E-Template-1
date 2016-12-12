74668676df8580a03e616203e6282201065c0d33
Brandon Espinoza <espinozab100@gmail.com>
2016-12-12 15:47:46 -0600

    Added paperclips settings

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,4 @@
 class Contact < ActiveRecord::Base
+  has_attached_file :contact_bg, styles: { large: "1920x1080>", medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :contact_bg, content_type: /\Aimage\/.*\z/
 end
