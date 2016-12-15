5efef965dee972b020f15ebcd0073ffddf1f7432
Brandon Espinoza <espinozab100@gmail.com>
2016-12-15 14:44:11 -0600

    Created Title Model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214173301) do
+ActiveRecord::Schema.define(version: 20161215204327) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "contact_title"
@@ -104,6 +104,12 @@ ActiveRecord::Schema.define(version: 20161214173301) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "titles", force: :cascade do |t|
+    t.string   "services_title"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "users", force: :cascade do |t|
