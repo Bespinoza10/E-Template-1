75ac8558d548b83e20a24288090650c687796ee5
Brandon Espinoza <espinozab100@gmail.com>
2017-01-19 13:55:04 -0600

    Added default img, wip

--- app/models/header.rb
+++ app/models/header.rb
@@ -1,4 +1,4 @@
 class Header < ActiveRecord::Base
-  has_attached_file :header_bg, styles: { large: "1920x1080>", medium: "300x300>", thumb: "100x100>" }, default_url: ":style/missing.png"
+  has_attached_file :header_bg, styles: { large: "1920x1080>", medium: "300x300>", thumb: "100x100>" }, default_url: "default/header-default.png"
   validates_attachment_content_type :header_bg, content_type: /\Aimage\/.*\z/
 end
