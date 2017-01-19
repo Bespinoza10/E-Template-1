8b23c659181e8c49d53bd9dd39b5b4f73dc85fd6
Brandon Espinoza <espinozab100@gmail.com>
2017-01-19 13:46:28 -0600

    WIP Default Image with paperclip

--- app/models/header.rb
+++ app/models/header.rb
@@ -1,4 +1,4 @@
 class Header < ActiveRecord::Base
-  has_attached_file :header_bg, styles: { large: "1920x1080>", medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :header_bg, styles: { large: "1920x1080>", medium: "300x300>", thumb: "100x100>" }, default_url: ":style/missing.png"
   validates_attachment_content_type :header_bg, content_type: /\Aimage\/.*\z/
 end
